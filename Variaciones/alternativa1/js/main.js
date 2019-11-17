1647b2924e12f8e781912c17c826ecc5999718eb
Alplox <42575576+Alplox@users.noreply.github.com>
2019-11-17 14:26:46 -0300

    Update main.js

--- Variaciones/alternativa1/js/main.js
+++ Variaciones/alternativa1/js/main.js
@@ -72,7 +72,6 @@ var App = {
         },
         'mi-radiols': {
             'nombre': '📻 Mi Radio LS <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>',
-            'status': 'offline',
             'code':'<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UClJiriqxFwmmVIQeASJfICQ&autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
         },
         'canal-9': {
