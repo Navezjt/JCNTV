b08f4388a98adccf14481a1e2d0a4a28a3c2d733
Alplox <42575576+Alplox@users.noreply.github.com>
2019-11-25 21:36:50 -0300

    Update main.js

--- Variaciones/alternativa1/js/main.js
+++ Variaciones/alternativa1/js/main.js
@@ -125,10 +125,10 @@ var App = {
         'c5n': {
             'nombre': 'C5N 🌍',
             'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UCFgk2Q2mVO1BklRQhSv6p0w&autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
- //       },
- //       'el-tiempo': {
- //           'nombre': 'EL TIEMPO 🌍',
- //           'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UCe5-b0fCK3eQCpwS6MT0aNw&autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+        },
+        'el-tiempo': {
+            'nombre': 'EL TIEMPO 🌍',
+            'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UCe5-b0fCK3eQCpwS6MT0aNw&autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
  //       },
  //        'revista-semana': {
  //           'nombre': 'Revista Semana 🌍',
