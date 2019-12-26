2a023f6d9b68f4688a7b1ed49f1ed8a524d31003
Alplox <42575576+Alplox@users.noreply.github.com>
2019-12-26 13:28:11 -0300

    Update canales.js

--- canales.js
+++ canales.js
@@ -25,8 +25,7 @@ var App = {
         },
         'meganoticias-2': {
             'nombre': 'Meganoticias 2 <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>', 
-            'status': 'offline',
-            'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/DfT1ifi3KEM?autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><img class="rec" src="https://github.com/Alplox/tele/raw/master/icons/rojo.png"><a href="https://www.youtube.com/channel/UCkccyEbqhhM3uKOI6Shm-4Q" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> Meganoticias</a> (Live 2)&nbsp;<img class="edge" src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></div>'
+            'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/XbXcgSeFW4Y?autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><img class="rec" src="https://github.com/Alplox/tele/raw/master/icons/rojo.png"><a href="https://www.youtube.com/channel/UCkccyEbqhhM3uKOI6Shm-4Q" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> Meganoticias</a> (Live 2)&nbsp;<img class="edge" src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></div>'
         },
         'meganoticias-3': {
             'nombre': 'Meganoticias 3 <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>',
@@ -321,6 +320,10 @@ var App = {
          'imuc-radio-chile': {
             'nombre': 'IMUC Chile <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img> 🎵',
             'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UCIIDtZoaK9UZi4FaGMmL_hw&autoplay=1&mute=0&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><img class="edge" src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img><a href="https://www.youtube.com/channel/UCIIDtZoaK9UZi4FaGMmL_hw" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> IMUC Chile</a> 🎵</div>'
+        },
+        'kultura': {
+            'nombre': 'KuLtura.cl <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img> 🎵',
+            'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UCV61cDcwrwFh35OsM8yWp3Q&autoplay=1&mute=0&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><img class="edge" src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img><a href="https://www.youtube.com/channel/UCV61cDcwrwFh35OsM8yWp3Q" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> KuLtura.cl</a> 🎵</div>'
         },
         'radio-placeres': {
             'nombre': '📻 Radio Placeres 87.7 FM <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>',
