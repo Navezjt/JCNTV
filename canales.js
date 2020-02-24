0954d88dfbe70cd203c253903f375d0c79fa87d0
Alplox <42575576+Alplox@users.noreply.github.com>
2020-02-23 23:45:39 -0300

    Update canales.js

--- canales.js
+++ canales.js
@@ -5,13 +5,13 @@ var App = {
             'nombre': '24 horas <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>', 
             'code': '<div class="embed-responsive embed-responsive-16by9"><iframe src="https://www.youtube.com/embed/live_stream?channel=UCTXNz3gjAypWp3EhlIATEJQ&autoplay=1&mute=1&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><img class="rec" src="https://github.com/Alplox/tele/raw/master/icons/rojo.png"><a href="https://www.youtube.com/channel/UCTXNz3gjAypWp3EhlIATEJQ" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> 24 Horas TVN</a>&nbsp;<img class="edge" src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></div>'
         },
-          'canal-13': {
+          'canal-13-m3u': {
             'nombre': 'Canal 13 <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>',
             'code': '<div class="embed-responsive embed-responsive-16by9"><iframe scrolling="no" src="https://alplox.github.io/tele/m3u/canal13" frameborder="0" hspace="0" vspace="0" marginheight="0" marginwidth="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><a href="https://www.13.cl/en-vivo?page=1" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> Canal 13 </a><img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img></div>'      
         },
-         'tv-chile': {
+         'tv-chile-m3u': {
             'nombre': 'TV Chile <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>',
-            'code': '<div class="embed-responsive embed-responsive-16by9"><iframe scrolling="no" src="https://alplox.github.io/tele/m3u/tv-chile" frameborder="0" hspace="0" vspace="0" marginheight="0" marginwidth="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><a href="https://www.tvchile.cl/envivo/" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> Canal 13 </a><img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img></div>'      
+            'code': '<div class="embed-responsive embed-responsive-16by9"><iframe scrolling="no" src="https://alplox.github.io/tele/m3u/tv-chile" frameborder="0" hspace="0" vspace="0" marginheight="0" marginwidth="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="nombre"><a href="https://www.tvchile.cl/envivo/" target="_blank" class="tooltip-test" title="Ir a la página oficial de esta emisión"> TV Chile </a><img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img></div>'      
         },
         'venticuatro-play': {
             'nombre': '24 Play <img src="https://github.com/Alplox/tele/raw/master/icons/chile.png"></img>', 
@@ -502,10 +502,11 @@ var App = {
         } else {
             App.add('t13');
             App.add('cnn-m3u');
-            App.add('canal-13');
+            App.add('canal-13-m3u');
             App.add('cima');
             App.add('cam-dipu-m3u-1');
             App.add('tv-senado-m3u');
+            App.add('tv-chile-m3u');
         }
         
     }
