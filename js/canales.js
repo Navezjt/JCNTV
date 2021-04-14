01be09a7d3f42f6c5433c356ddaefe4efaf2aadc
Alplox <42575576+Alplox@users.noreply.github.com>
2021-04-13 20:10:28 -0400

    scripts separados
    espero no habermelo pitiado

--- js/canales.js
+++ js/canales.js
@@ -1,50 +1,3 @@
-// barra tamaño https://codepen.io/nosurprisethere/pen/KJxwQz
-const settings = {
-    fill: '#ce6102',
-    background: '#1f1f1f'
-}
-
-const sliders = document.querySelectorAll('.range-slider');
-
-Array.prototype.forEach.call(sliders, (slider) => {
-    slider.querySelector('input').addEventListener('input', (event) => {
-        slider.querySelector('span').innerHTML = event.target.value;
-        applyFill(event.target);
-    });
-    applyFill(slider.querySelector('input'));
-});
-
-function applyFill(slider) {
-    const percentage = 100 * (slider.value - slider.min) / (slider.max - slider.min);
-    const bg = `linear-gradient(90deg, ${settings.fill} ${percentage}%, ${settings.background} ${percentage + 0.1}%)`;
-    slider.style.background = bg;
-}
-/////////////////////////////////////////////////////////
-
-///Filtro de canales https://www.w3schools.com/bootstrap/bootstrap_filters.asp
-$(document).ready(function () {
-    $("#mifiltro").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        $("#PorFiltrar *").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});
-////////////////////////////////////////////////////////
-
-///copiar enlace a portapapeles//////////////////////////
-function copiarEnlace() {
-    var copyText = document.getElementById("myInput");
-
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-    document.execCommand("copy");
-
-    alert("Copiado exitoso!: " + copyText.value);
-}
-/////////////////////////////////////////////////////////
-
 ///canales/////////////////////////////////////////////////////////
 var size = 4;
 var sizeMobile = 12;
@@ -670,11 +623,4 @@ anchoStreams.oninput = function (event) {
     let streams2 = document.getElementById("los-canales");
     streams2.style.maxWidth = event.target.value + "%"
 }
-/////////////////////////////////////////////////////////
-///nombre transmisiones on/off https://www.w3schools.com/jquery/jquery_hide_show.asp/////////
-$(document).ready(function(){
-    $(".btn1-nombres-stream").click(function(){
-      $(".nombre-barra").toggle();
-    });
-  });
 /////////////////////////////////////////////////////////
