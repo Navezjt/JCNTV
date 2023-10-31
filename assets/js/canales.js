b6797fd5e16b034a13de2af7c1ef37d5866e52d3
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-10-31 10:57:34 -0500

    Update canales.js

--- assets/js/canales.js
+++ assets/js/canales.js
@@ -198,7 +198,7 @@ const channels = {
     },
     'rn-tv': {
         'name': 'Radio Noticias Televisión (RN) <img src="assets/svg/paises/peru.svg" title="Peru"><span hidden>Peru, Yurimaguas, español</span>',
-        'iframe': 'hhttps://videoserver.tmcreativos.com:2020/VideoPlayer/rnyurimaguas?autoplay=1',
+        'fuente': 'hhttps://videoserver.tmcreativos.com:2020/VideoPlayer/rnyurimaguas?autoplay=1',
         'source': 'http://radionoticiastv.com/'
     },
     'vz-tv': {
