f8b0843faa8c6d6efac254e693e019015773e6b9
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2024-11-05 15:16:35 -0500

    Update canales.js

--- assets/js/canales.js
+++ assets/js/canales.js
@@ -275,16 +275,6 @@ const channels = {
         'm3u': 'https://live.obslivestream.com/norselvatv/index.m3u8',
         'source': 'https://radiotvnorselva.com/'
     },
-    'sanjuaneratv': {
-        'name': 'Sanjuaneratv <img src="assets/svg/paises/peru.svg" title="Peru"><span hidden>Perú, español</span>',
-        'm3u': 'https://infosmartpe.store:5443/LiveApp/streams/vLn2BEFdTBNu8866060423704237.m3u8',
-        'source': 'http://sanjuaneratv.pe/'
-    },
-    'sanjuanera-tv': {
-        'name': 'Sanjuanera TV <img src="assets/svg/paises/peru.svg" title="Peru"><span hidden>Perú, español</span>',
-        'code': 'https://infosmartpe.store:5443/LiveApp/play.html?id=vLn2BEFdTBNu8866060423704237',
-        'source': 'http://sanjuaneratv.pe/'
-    },
     'supercanal': {
         'name': 'Supercanal <img src="assets/svg/paises/peru.svg" title="Peru"><span hidden>Perú, español</span>',
         'm3u': 'https://live.obslivestream.com/supercanal/index.m3u8',
